8dd53e6c6f59e87e6690c91e55105054e90dccab
Devrajsinh Jethwa <jethwadevrajsinh2005@gmail.com>
2025-02-22 14:49:50 +0530

    Added Images of application.

--- Frontend/src/components/HowItWorks.jsx
+++ Frontend/src/components/HowItWorks.jsx
@@ -8,7 +8,7 @@ const HowItWorks = () => {
   return (
     <section className='my-8'>
         <h1 className='text-center text-3xl font-semibold'>How It Works</h1>
-        <aside className='mt-8 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4 *:flex *:flex-col *:items-center *:border-2 *:border-black *:rounded-2xl *:bg-gray-900 max-[540px]:grid-cols-1 *:shadow-sm *:shadow-gray-200'>
+        <aside className='mt-8 grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] max-[1525px]:grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4 *:flex *:flex-col *:items-center *:border-2 *:border-black *:rounded-2xl *:bg-gray-900 max-[540px]:grid-cols-1 *:shadow-sm *:shadow-gray-200'>
             <div>
                 <span className='bg-[#8136E2] inline-block rounded-2xl m-2'><FaHandPointUp className='m-2'/></span>
                 <h3 className='text-md font-medium'>Book in just 2 Taps</h3>
